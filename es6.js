ce769cab4c49a3ece62b6028f56131d473b7e7a6
jkhan2211 <junaidkhandev123@gmail.com>
2022-02-10 23:16:28 -0500

    Added to my proj

---
+++ es6.js
@@ -0,0 +1,36 @@
+// Javascript Mastery = JS ES6 For beginner
+
+// const and let
+// let - changing
+
+let age = 20 // can be reassign
+const name = "Junaid"
+
+console.log(age);
+
+age = age + 1;
+
+console.log(age)
+
+// arrow function
+// implicit return
+
+const add = (a, b) => {
+    console.log(a + b);
+    
+
+}
+add(2, 2);
+
+// template string
+
+
+// default parameters
+
+// array destruction
+
+//object destruction
+
+//rest operator
+
+// spread operator 

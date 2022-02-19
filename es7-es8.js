86fc9372286855562e0cea458479b1f5b46d2567
jkhan2211 <junaidkhandev123@gmail.com>
2022-02-19 00:56:12 -0500

    es7-es8

--- es7-es8.js
+++ es7-es8.js
@@ -1,8 +1,31 @@
 //String.prototype.padStart/PadEnd
 
+const string = '12345';
+
+console.log(string.padStart(10, '.....'));
+console.log(string.padEnd(10, '......'));
 
 // Object.values
+
+const object = {
+    name: "John",
+    age: 20,
+    favouriteBooks : [ "Harry Potter 1, Harry Potter 2"],
+}
+
+console.log(Object.values(object));
 // Object.entries
-// Async Functions
+
+console.log(Object.entries(object))
 // Exponentiation
+Math.pow(2, 3);
+console.log(2 ** 3);
+
 // Trailing Commas
+const anotherObject = {
+    first: 1,
+    second: 2, 
+    third: 3,
+}
+
+const array = [1, 2, 3, ,];
